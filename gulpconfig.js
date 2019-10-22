26ddbc7e2c8387d3da5113b36d60caee7376b481
liumemgmei <18768147259@163.com>
2019-10-22 15:47:31 +0800

    由中间层模板生成前端代码块

---
+++ gulpconfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+  codePath: '../ems-view2/src/routes',
+  typesPath:'../ems-view2/src'
+}
---
+++ templates/model.js
@@ -0,0 +1,11 @@
+
+//必须包含字段id
+module.exports = [{
+    filepath: '/login/',
+    data: {
+      model:[{
+        name: 'a1111'
+      }]
+    }
+  }]
+
