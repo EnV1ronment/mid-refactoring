e8c4c86ded89fc37ba1519e409f660b140a0c84c
zwfgogo <wf_want_fly@163.com>
2019-10-22 09:10:41 +0800

    态势感知设计

---
+++ src/routes/awareness/load.ts
@@ -0,0 +1,26 @@
+import {NextFunction, Router} from "express";
+import {IRequest, IResponse} from "../../types/globals";
+import * as requestHandler from "../../public/handler/request_handler";
+import * as QRcodeControllers from "../../controllers/QR_code/QR_codeControllers";
+
+const router = Router();
+/**
+ * @api {get} /awareness/load/curve 负荷功率曲线
+ * @apiVersion 0.0.0
+ * @apiGroup Situational awareness
+ * @apiDescription 获取负荷功率曲线
+ * @apiHeader {String} access-token 用户Token
+ * @apiSampleRequest http://192.168.2.86:3001/awareness/load/curve
+ * @apiSuccessExample {json} Success-Response:
+ { "content": "This is an example content" }
+ */
+router.get('/curve', async (req: IRequest, res: IResponse, next: NextFunction) => {
+    try {
+        const reply:any = await QRcodeControllers.getQRcode(req);
+        requestHandler.responseHandle(res, reply);
+    } catch (e) {
+        next(e);
+    }
+});
+
+export default router;
